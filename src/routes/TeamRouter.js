506652d3be90c81e8364d6a933738c839dbdc8f2
GeorgeValle <jorgeguillermovalle@gmail.com>
2023-04-24 02:37:56 -0300

    Refactor: modified MongoDao, TeamController, TeamService and team router
    complet incompletes methods

--- src/routes/TeamRouter.js
+++ src/routes/TeamRouter.js
@@ -33,22 +33,22 @@ routes.patch('/goals/:id_team/:goals', team.incrementOneTeamGoals)
 routes.get('/goals/:id_team', team.getOneTeamGoals)
 
 //update score of wins of one team
-routes.patch('/ud_wins/:id_team', team.updateOneTeamWins)
+routes.patch('/ud_wins/:id_team/:wins', team.updateOneTeamWins)
 
 //increment score of wins of one team
-routes.patch('/wins/:id_team', team.incrementOneTeamWins)
+routes.patch('/wins/:id_team/:wins', team.incrementOneTeamWins)
 
 //update one team losses
-routes.patch('/ud_losses/:id_team', team.updateOneTeamLosses)
+routes.patch('/ud_losses/:id_team/:losses', team.updateOneTeamLosses)
 
 //increment one team losses
-routes.patch('/losses/:id_team', team.incrementOneTeamLosses)
+routes.patch('/losses/:id_team/:losses', team.incrementOneTeamLosses)
 
 //update one team points
-routes.patch('/ud_points/:id_team', team.updateOneTeamPoints)
+routes.patch('/ud_points/:id_team/points', team.updateOneTeamPoints)
 
 //increment one team points
-routes.patch('/losses/:id_team', team.incrementOneTeamPoints)
+routes.patch('/points/:id_team/:points', team.incrementOneTeamPoints)
 
 
 
